7aeb547ea878f4e3797379f24af6dfb3770279c4
Ko Sehoon <122508622+Gobro-s@users.noreply.github.com>
2023-10-26 13:03:52 +0900

    bottomTab 적용 (#7)

---
+++ screens/HomeScreen/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Button} from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Button title="HomeScreenDemo" />
+    </View>
+  );
+};
+
+export default HomeScreen;
