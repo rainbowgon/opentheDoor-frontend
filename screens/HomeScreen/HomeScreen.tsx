76ac22e4c471b04681e4a49a0bfbaf3547c2a14d
gobro <gobro6366@gmail.com>
2023-10-27 11:20:36 +0900

    make input component and fab component

--- screens/HomeScreen/HomeScreen.tsx
+++ screens/HomeScreen/HomeScreen.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import {View, Button} from 'react-native';
+import Input from '../../components/input/Input';
+import CustomButton from '../../components/button/CustomButton';
+import CustomFab from '../../components/fab/Fab';
+import ListItem from '../../components/listItem/ListItem';
 
 const HomeScreen = () => {
   return (
     <View>
       <Button title="HomeScreenDemo" />
+      <Input customProp="example" onChangeText={text => console.log(text)} />
+      <CustomButton>Press me</CustomButton>
+      <CustomFab />
+      <ListItem />
     </View>
   );
 };
