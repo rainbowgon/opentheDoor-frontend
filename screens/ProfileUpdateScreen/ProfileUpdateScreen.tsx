0e8df9ca215ab5a85a60bb50c67a566f9583c872
leejm9704 <leejm9704@naver.com>
2023-11-03 09:13:29 +0900

    [feat] ProfileUpate 페이지 구조 제작

--- screens/ProfileUpdateScreen/ProfileUpdateScreen.tsx
+++ screens/ProfileUpdateScreen/ProfileUpdateScreen.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
-import { View, Button, ScrollView } from 'react-native';
+import { View, Button, ScrollView, Image } from 'react-native';
+
+// image
+import ImageDefault from "../../assets/images/image-default.png";
+import CustomButton from '../../components/Button/CustomButton';
+import Input from '../../components/Input/Input';
 
 const ProfileUpdateScreen = () => {
   return (
     <ScrollView>
       <Button title="ProfileUpdateScreenDemo" />
+      <Image source={ImageDefault} />
+      <CustomButton
+        value="수정하기"
+      />
+      <Input></Input>
+      <Input></Input>
+      <CustomButton
+        value="인증번호"
+      />
+      <Input></Input>
+      <Input></Input>
+      <Input></Input>
+      <CustomButton
+        value="프로필 수정"
+      />
     </ScrollView>
   );
 };
