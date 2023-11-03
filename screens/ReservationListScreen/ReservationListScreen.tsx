44ce0c00141e5f4a16178630bb40984f077295d4
gobro <gobro6366@gmail.com>
2023-11-03 13:24:56 +0900

    [feat] 예약 내역 페이지 레이아웃

--- screens/ReservationListScreen/ReservationListScreen.tsx
+++ screens/ReservationListScreen/ReservationListScreen.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
-import { View, Button, ScrollView } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { View, Button, ScrollView } from "react-native";
+import Header from "../../components/Header/Header";
+import InfoCard from "../../components/InfoCard/InfoCard";
 
 const ReservationListScreen = () => {
+  // const [reservations, setReservations] = useState([]);
+
+  // useEffect(() => {
+  //   fetch('API명세서가/비어/있군요/NgLee')
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       setReservations(data);
+  //     })
+  //     .catch(error => {
+  //       console.error('못가져옴!', error);
+  //     });
+  // }, []);
   return (
     <ScrollView>
       <Button title="ReservationListScreenDemo" />
+      <Header />
+      {/* {reservations.map((reservation, index) => (
+        <InfoCard key={index} theme={reservation.title} />
+      ))} */}
+      <InfoCard
+        theme="백조의 호수"
+        branch="역삼 익스케이프"
+        time="2023.11.03(금) 19:00 ~"
+        reviewCount={5}
+        price={120000}
+        memberCount={4}
+      />
     </ScrollView>
   );
 };
