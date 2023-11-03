d607b858f76fe17eb3f03c8ed51fbd8e46c9c63d
leejm9704 <leejm9704@naver.com>
2023-11-03 11:28:54 +0900

    [feat] ReservationWating 페이지 구조 제작

--- screens/ReservationWaitingScreen/ReservationWaitingScreen.tsx
+++ screens/ReservationWaitingScreen/ReservationWaitingScreen.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { View, Button, ScrollView } from 'react-native';
+import InfoCard from '../../components/InfoCard/InfoCard';
 
 const ReservationWaitingScreen = () => {
   return (
     <ScrollView>
       <Button title="ReservationWaitingScreenDemo" />
+      <View>
+        {
+          [...Array(3)].map(() => (
+            <InfoCard />
+          ))
+        }
+      </View>
     </ScrollView>
   );
 };
