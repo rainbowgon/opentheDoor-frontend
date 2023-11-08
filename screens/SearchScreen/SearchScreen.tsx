dd21e8be071d2870026b8bb6e1d2d71e36827020
Ko Sehoon <122508622+Gobro-s@users.noreply.github.com>
2023-11-08 12:21:33 +0900

    [Feat] HomeScreen axios test 준비 (#66)

--- screens/SearchScreen/SearchScreen.tsx
+++ screens/SearchScreen/SearchScreen.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {View, Button} from 'react-native';
+import React from "react";
+import { View, Button } from "react-native";
 
 const SearchScreen = () => {
   return (
