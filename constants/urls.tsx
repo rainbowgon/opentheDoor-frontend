1033254f3b70f117ac7259a42af0f904006d170b
gobro <gobro6366@gmail.com>
2023-11-10 16:08:43 +0900

    [feat] 검색 관련 테스트용 API_URL 추가

--- constants/urls.tsx
+++ constants/urls.tsx
@@ -4,8 +4,11 @@ export const SERVER_URL = "http://localhost:3000";
 // export const API_URL = "https://rainbowgon.com";
 
 // FIXME 테스트 서버용
-export const API_URL =
-  "http://ssafy-openthedoor-alb-595590811.ap-northeast-2.elb.amazonaws.com";
+// export const API_URL =
+//   "http://ssafy-openthedoor-alb-595590811.ap-northeast-2.elb.amazonaws.com";
+
+// NOTE 변서버
+export const API_URL = "70.12.247.254";
 
 // 로컬 서버용
 // export const API_URL = "http://localhost:8080";
