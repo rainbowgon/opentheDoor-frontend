b908e2cf815ace7a35c1aac7dc83d81c5cfe6874
gobro <gobro6366@gmail.com>
2023-11-13 14:00:06 +0900

    [feat] HomeScreen의 지도에서 다중 Marker

--- screens/HomeScreen/components/NearByTheme/NearByTheme.tsx
+++ screens/HomeScreen/components/NearByTheme/NearByTheme.tsx
@@ -11,7 +11,139 @@ import SearchScreen from "../../../SearchScreen/SearchScreen";
 import { PermissionsAndroid } from "react-native";
 import { Region, Marker } from "react-native-maps";
 
+// apis
+import getThemeSearch from "../../../../recoil/theme/themeFeature";
+
 const NearByTheme = () => {
+  const [markers, setMarkers] = useState([]);
+  const [selectedMarker, setSelectedMarker] = useState(null);
+  const [modalVisible, setModalVisible] = useState(false);
+  const handleMarkerPress = markerData => {
+    setSelectedMarker(markerData);
+    setModalVisible(true);
+  };
+
+  ///dummy
+  useEffect(() => {
+    // 더미 데이터를 사용하여 마커 설정
+    const dummyData = {
+      data: [
+        {
+          id: "eTHntosBdUGVeOMUXDR3",
+          venue: "마스터키 플레이포인트랩강남점",
+          title: "리허설",
+          poster:
+            "https://ssafy-otd-public.s3.ap-northeast-2.amazonaws.com/masterkey/리허설_84169924.gif",
+          level: 4.0,
+          minHeadcount: 2,
+          maxHeadcount: 6,
+          priceList: null,
+          timeLimit: null,
+          latitude: 37.5008831,
+          longitude: 127.027485,
+        },
+        {
+          id: "dTHntosBdUGVeOMUQDTg",
+          venue: "마스터키 해운대 블루오션스테이션",
+          title: "리허설",
+          poster:
+            "https://ssafy-otd-public.s3.ap-northeast-2.amazonaws.com/masterkey/리허설_23761838.gif",
+          level: 4.0,
+          minHeadcount: 2,
+          maxHeadcount: 6,
+          priceList: null,
+          timeLimit: null,
+          latitude: 35.1629768,
+          longitude: 129.158492,
+        },
+        {
+          id: "PppbxosB2bv_Fhtj6wSF",
+          venue: "마스터키 천안두정점",
+          title: "경찰서를 털어라",
+          poster:
+            "https://ssafy-otd-public.s3.ap-northeast-2.amazonaws.com/masterkey/경찰서를 털어라_73260549.jpg",
+          level: 3.0,
+          minHeadcount: 2,
+          maxHeadcount: 6,
+          priceList: null,
+          timeLimit: null,
+          latitude: 36.8339795,
+          longitude: 127.1367309,
+        },
+        {
+          id: "9JpbxosB2bv_FhtjhAOC",
+          venue: "마스터키 강남프라임",
+          title: "Do The G",
+          poster:
+            "https://ssafy-otd-public.s3.ap-northeast-2.amazonaws.com/masterkey/Do The G_21189403.png",
+          level: 3.0,
+          minHeadcount: 2,
+          maxHeadcount: 4,
+          priceList: null,
+          timeLimit: null,
+          latitude: 37.5008831,
+          longitude: 127.027485,
+        },
+        {
+          id: "GZpbxosB2bv_FhtjswSL",
+          venue: "마스터키 천안신부점",
+          title: "터널",
+          poster:
+            "https://ssafy-otd-public.s3.ap-northeast-2.amazonaws.com/masterkey/터널_52402111.jpg",
+          level: 3.0,
+          minHeadcount: 2,
+          maxHeadcount: 6,
+          priceList: null,
+          timeLimit: null,
+          latitude: 36.8183413,
+          longitude: 127.1553078,
+        },
+        {
+          id: "9ZpbxosB2bv_FhtjhAOC",
+          venue: "마스터키 강남프라임",
+          title: "어웨이큰",
+          poster:
+            "https://ssafy-otd-public.s3.ap-northeast-2.amazonaws.com/masterkey/어웨이큰_14869136.png",
+          level: 5.0,
+          minHeadcount: 2,
+          maxHeadcount: 5,
+          priceList: null,
+          timeLimit: null,
+          latitude: 37.5008831,
+          longitude: 127.027485,
+        },
+        {
+          id: "G5pbxosB2bv_FhtjswSL",
+          venue: "마스터키 천안신부점",
+          title: "트러브메이커",
+          poster:
+            "https://ssafy-otd-public.s3.ap-northeast-2.amazonaws.com/masterkey/트러브메이커_57557775.jpg",
+          level: 3.0,
+          minHeadcount: 2,
+          maxHeadcount: 6,
+          priceList: null,
+          timeLimit: null,
+          latitude: 36.8183413,
+          longitude: 127.1553078,
+        },
+      ],
+    };
+    setMarkers(dummyData.data);
+  }, []);
+
+  ///////////////////////////////axios
+  // useEffect(() => {
+  //   const fetchMarkers = () => {
+  //     // getThemeSearch 함수 호출
+  //     getThemeSearch();
+  //     // keyword: "키워드",
+  //     // page: 1,
+  //     // size: 10,
+  //   };
+
+  //   fetchMarkers();
+  // }, []);
+
   // default를 서울로 지정했습니다!
   const [region, setRegion] = useState<Region>({
     latitude: 37.5665,
@@ -68,7 +200,18 @@ const NearByTheme = () => {
       <Text>내 주변</Text>
       <View style={styles.mapContainer}>
         <CustomMap region={region} style={styles.map}>
-          <Marker coordinate={region} title={"내 위치"} />
+          {/* 각 마커에 클릭 이벤트 연결 */}
+          {markers.map((markerData, index) => (
+            <Marker
+              key={index}
+              coordinate={{
+                latitude: markerData.latitude,
+                longitude: markerData.longitude,
+              }}
+              title={markerData.title}
+              onPress={() => handleMarkerPress(markerData)}
+            />
+          ))}
         </CustomMap>
         <CustomFab
           icon={Zoomicon}
