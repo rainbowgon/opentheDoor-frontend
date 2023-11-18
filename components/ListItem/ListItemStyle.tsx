aaea3436de28f5c4b2749ba7393c76b386ec6f04
leejm9704 <leejm9704@naver.com>
2023-11-18 22:46:12 +0900

    [style] 페이지 디자인 (마이페이지, 설정) 및 데이터 연결
    - 마이페이지 (MypageScreen)
    - 설정 (SettingScreen)

--- components/ListItem/ListItemStyle.tsx
+++ components/ListItem/ListItemStyle.tsx
@@ -1,8 +1,18 @@
 import styled from 'styled-components/native';
 import { theme } from "../../styles/colors";
 
+const handleColor = (props) => {
+  if (props.color === "disable") return theme.semantic_disable;
+  if (props.color === "error") return theme.semantic_error;
+  if (props.color === "info") return theme.semantic_info;
+  if (props.color === "warn") return theme.semantic_warn;
+  if (props.color === "success") return theme.semantic_success;
+  return theme.font2;
+}
+
 export const ListItemView = styled.TouchableOpacity`
   margin: 5px;
+  padding: 0 25px;
   height: 60px;
   display: flex;
   flex-direction: row;
@@ -12,14 +22,14 @@ export const ListItemView = styled.TouchableOpacity`
 `;
 
 export const ListItemIcon = styled.Image`
-  height: 35px;
-  width: 35px;
+  height: 25px;
+  width: 25px;
   margin-right: 10px;
+  tint-color: ${theme.font5};
   
 `;
 
 export const ListItemItem = styled.View`
-  margin: 20px;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
@@ -27,10 +37,10 @@ export const ListItemItem = styled.View`
 `;
 
 export const ListItemTitle = styled.Text`
-  color: ${theme.font2};
-  font-weight: 700;
+  color: ${handleColor};
+  font-size: 16px;
   `;
 
 export const ListItemContent = styled.Text`
-  color: ${theme.font3};
+  color: ${theme.font5};
 `;
---
+++ screens/MypageScreen/MypageScreenStyle.tsx
@@ -0,0 +1,8 @@
+import { theme } from "../../styles/colors";
+import styled from "styled-components/native";
+
+// export const WeeklyThemeStyle = styled.TouchableOpacity``;
+
+export const MypageContainer = styled.View`
+  margin: 0 20px;
+`;
