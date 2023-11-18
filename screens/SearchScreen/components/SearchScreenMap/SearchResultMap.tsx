e125af26be1a3d087302c841721b173b8eef5c16
gobro <gobro6366@gmail.com>
2023-11-19 04:12:46 +0900

    [feat] infocard 띄우기까지 완료

--- screens/SearchScreen/components/SearchScreenMap/SearchResultMap.tsx
+++ screens/SearchScreen/components/SearchScreenMap/SearchResultMap.tsx
@@ -9,47 +9,25 @@ import {
 import { Marker } from "react-native-maps";
 import Geolocation from "@react-native-community/geolocation";
 import CustomMap from "../../../../components/Maps/Map";
-import InfoCard from "../../../../components/InfoCard/InfoCard";
 import { useRecoilValue } from "recoil";
 import { searchResultsState } from "../../../../recoil/search/search";
+import InfoCard from "../../../../components/InfoCard/InfoCard";
+import CustomButton from "../../../../components/Button/CustomButton";
 
 const SearchResultMap = () => {
-  const [searchText, setSearchText] = useState("");
   const searchResults = useRecoilValue(searchResultsState);
+  const [selectedMarkerData, setSelectedMarkerData] = useState(null);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [listModalVisible, setListModalVisible] = useState(false);
   const [region, setRegion] = useState({
     latitude: 37.5013,
     longitude: 127.0396781,
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
   });
-  const [selectedMarkerData, setSelectedMarkerData] = useState(null);
-  const [modalVisible, setModalVisible] = useState(false);
-
-  const renderMarkers = () => {
-    return searchResults.map((result, index) => (
-      <Marker
-        key={index}
-        coordinate={{
-          latitude: result.latitude,
-          longitude: result.longitude,
-        }}
-        title={result.title || "검색 위치"}
-        // 추가적인 마커 옵션
-      />
-    ));
-  };
-  const executeSearch = async () => {
-    try {
-      const results = await getThemeSearch({ keyword: searchText });
-      setSearchResults(results);
-    } catch (error) {
-      console.error("테마 검색 중 오류 발생", error);
-    }
-  };
 
   useEffect(() => {
-    // 요청한 위치 정보 접근 권한
-    const requestLocationPermission = async () => {
+    async function requestLocationPermission() {
       const granted = await PermissionsAndroid.request(
         PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
         {
@@ -65,36 +43,63 @@ const SearchResultMap = () => {
       } else {
         console.log("위치 정보 접근 권한 거부됨");
       }
-    };
+    }
 
     requestLocationPermission();
   }, []);
 
   useEffect(() => {
-    // 현재 위치 정보 가져오기
-    Geolocation.getCurrentPosition(
-      position => {
-        setRegion({
-          ...region,
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-        });
-      },
-      error => console.error("위치 정보를 받아오는데 실패했습니다.", error),
-      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
-    );
-  }, []);
+    if (searchResults.length > 0) {
+      // 모든 마커의 위치를 기준으로 중심 위치 계산
+      let minLat = Number.MAX_VALUE;
+      let maxLat = -Number.MAX_VALUE;
+      let minLng = Number.MAX_VALUE;
+      let maxLng = -Number.MAX_VALUE;
 
-  const handleMarkerPress = markerData => {
-    setSelectedMarkerData(markerData);
+      searchResults.forEach(item => {
+        minLat = Math.min(minLat, item.latitude);
+        maxLat = Math.max(maxLat, item.latitude);
+        minLng = Math.min(minLng, item.longitude);
+        maxLng = Math.max(maxLng, item.longitude);
+      });
+
+      const midLat = (minLat + maxLat) / 2;
+      const midLng = (minLng + maxLng) / 2;
+      const latDelta = maxLat - minLat;
+      const lngDelta = maxLng - minLng;
+
+      setRegion({
+        latitude: midLat,
+        longitude: midLng,
+        latitudeDelta: latDelta,
+        longitudeDelta: lngDelta,
+      });
+    }
+  }, [searchResults]);
+
+  const handleMarkerPress = item => {
+    setSelectedMarkerData(item);
     setModalVisible(true);
   };
 
   return (
     <View style={styles.container}>
-      <CustomMap region={region} style={{ height: 1000 }}>
-        {renderMarkers()}
-      </CustomMap>
+      {region && (
+        <CustomMap region={region} style={styles.map}>
+          {searchResults.map((item, index) => (
+            <Marker
+              key={index}
+              coordinate={{
+                latitude: item.latitude,
+                longitude: item.longitude,
+              }}
+              title={item.title}
+              pinColor="#BAB2FFFF"
+              onPress={() => handleMarkerPress(item)}
+            />
+          ))}
+        </CustomMap>
+      )}
 
       {selectedMarkerData && (
         <Modal
@@ -110,11 +115,19 @@ const SearchResultMap = () => {
               <InfoCard
                 themeId={selectedMarkerData.themeId}
                 title={selectedMarkerData.title}
+                // 필요한 다른 props 추가
               />
             </View>
           </TouchableOpacity>
         </Modal>
       )}
+      <View style={styles.listbutton}>
+        <CustomButton
+          mode="selected"
+          value="리스트로 보기"
+          onPress={() => setListModalVisible(true)}
+        />
+      </View>
     </View>
   );
 };
@@ -125,28 +138,45 @@ const styles = StyleSheet.create({
   },
   map: {
     ...StyleSheet.absoluteFillObject,
+    height: 1000,
   },
   modalView: {
     position: "absolute",
     bottom: 0,
     width: "100%",
+    // backgroundColor: "white",
+    // borderTopLeftRadius: 20,
+    // borderTopRightRadius: 20,
     padding: 20,
     height: 350,
-    maxHeight: 400,
-    backgroundColor: "white",
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20,
+    maxHeight: 400, // 또는 구체적인 높이 값으로 설정, 예: 200
+    // shadowColor: "#000",
     shadowOffset: {
       width: 0,
-      height: -2,
+      height: -2, // 그림자 방향 (위로)
     },
     shadowOpacity: 0.1,
     shadowRadius: 4,
+    // elevation: 20,
+  },
+  modalContainer: {
+    position: "absolute", // 절대 위치
+    bottom: 0, // 화면 하단에 위치
+    left: 0, // 왼쪽 정렬
+    right: 0, // 오른쪽 정렬
+    height: "30%", // 모달의 높이 (화면의 30%를 차지)
+    backgroundColor: "white", // 배경색
+    borderTopLeftRadius: 20, // 상단 왼쪽 모서리 둥글게
+    borderTopRightRadius: 20, // 상단 오른쪽 모서리 둥글게
+    padding: 20, // 내부 여백
   },
   modalOverlay: {
     flex: 1,
     justifyContent: "flex-end",
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    // backgroundColor: "rgba(0, 0, 0, 0.5)", // 반투명 배경
+  },
+  listbutton: {
+    bottom: 100,
   },
 });
 
