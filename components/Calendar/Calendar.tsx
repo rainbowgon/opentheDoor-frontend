0dfe63be9eff9a99d89f4e97c8abcb5b1cae8dde
leejm9704 <leejm9704@naver.com>
2023-11-20 02:03:49 +0900

    [feat] theme 디테일 페이지 및 데이터 정리

--- components/Calendar/Calendar.tsx
+++ components/Calendar/Calendar.tsx
@@ -1,6 +1,7 @@
 import React, { Component, useEffect, useState } from 'react';
 import {
   AppRegistry,
+  Text,
   View
 } from 'react-native';
 import moment from 'moment';
@@ -9,31 +10,67 @@ import { theme } from "../../styles/colors";
 import CalendarStrip from 'react-native-calendar-strip';
 import CustomButton from "../Button/CustomButton";
 import { TimeListView } from "./CalendarStyle";
+import { themeState } from "../../recoil/theme/theme";
+import { useRecoilValue } from "recoil";
 
 let datesWhitelist = [{
   start: moment(),
-  end: moment().add(3, 'days')  // total 4 days enabled
+  end: moment().add(7, 'days')
 }];
 
-let datesBlacklist = [moment().add(2, 'days')]; // 1 day disabled
-
-const curDate = new Date;
+let datesBlacklist = [];
 
 const Calendar = () => {
-  // useEffect(() => {
-  //   console.log(curDate);
-  // }, [curDate])
-  const [date, setDate] = useState(new Date);
 
-  const handleDate = (date) => {
-    setDate(date);
-    console.log(date);
+  const themeDetail = useRecoilValue(themeState);
+  // useEffect(() => {
+  //   console.log("dateList", dateList);
+  // }, [])
+  const [curDate, setCurDate] = useState(moment());
+  const [compareDate, setCompareDate] = useState({
+    compareToFullYear: new Date().getFullYear().toString(),
+    compareToMonth: (new Date().getMonth() + 1).toString(),
+    compareToDate: new Date().getDate().toString(),
+  });
+
+  const handleDate = (selectedDate) => {
+    setCurDate(selectedDate);
+  }
+
+  useEffect(() => {
+    console.log(curDate, typeof (curDate));
+    setCompareDate({
+      compareToFullYear: curDate.year().toString(),
+      compareToMonth: (curDate.month() + 1).toString(),
+      compareToDate: curDate.date().toString(),
+    });
+  }, [curDate])
+
+  const onCompareDate = (date: string) => {
+
+    const selectFullYear = date.substring(0, 4);
+    const selectMonth = date.substring(5, 7);
+    const selectDate = date.substring(8, 10);
+
+    if (selectFullYear !== compareDate.compareToFullYear) return false;
+    if (selectMonth !== compareDate.compareToMonth) return false;
+    if (selectDate !== compareDate.compareToDate) return false;
+
+    return true;
+  }
+
+  const onModeSelected = (isAvailable: string) => {
+    if (isAvailable === "AVAILABLE") {
+      return "selected";
+    }
+    return "inactive";
   }
 
   return (
     <View>
       <CalendarStrip
         selectedDate={curDate}
+        scrollable={true}
         onDateSelected={handleDate}
         calendarAnimation={{ type: 'parallel', duration: 20 }}
         // daySelectionAnimation={{ type: 'border', duration: 200, borderWidth: 1, borderHighlightColor: 'white' }}
@@ -54,16 +91,18 @@ const Calendar = () => {
         iconContainer={{ flex: 0.1 }}
       />
       <TimeListView>
-        <CustomButton mode="selected" value="07:00" />
-        <CustomButton mode="selected" value="08:30" />
-        <CustomButton mode="selected" value="10:00" />
-        <CustomButton mode="inactive" value="11:30" />
-        <CustomButton mode="selected" value="13:00" />
-        <CustomButton mode="inactive" value="14:30" />
-        <CustomButton mode="selected" value="16:00" />
-        <CustomButton mode="inactive" value="17:30" />
-        <CustomButton mode="selected" value="19:00" />
-        <CustomButton mode="inactive" value="20:30" />
+        {
+          themeDetail?.timeSlotList && themeDetail?.timeSlotList.map(timeInfo => (
+            <>
+              {
+                timeInfo.date && onCompareDate(timeInfo.date) &&
+                timeInfo.timeList?.map((time) => (
+                  <CustomButton mode={onModeSelected(time.isAvailable)} value={time.time} />
+                ))
+              }
+            </>
+          ))
+        }
       </TimeListView>
     </View >
   )
--- components/Review/ReviewItem.tsx
+++ components/Review/ReviewItem.tsx
@@ -1,5 +1,5 @@
 import { View, Text, Image } from 'react-native';
-import { ReviewContainer, ReviewContent, ReviewIcon, ReviewSubContent, ReviewTitle, ReviewUserContainer, ReviewUserImage, ReviewUserInfo, ReviewUserInfoItem, ReviewUserPrivacy, ReviewUserPrivacyInfo } from './ReviewStyle';
+import { ReviewContainer, ReviewContent, ReviewContentDisabled, ReviewIcon, ReviewSubContent, ReviewTitle, ReviewUserContainer, ReviewUserImage, ReviewUserInfo, ReviewUserInfoItem, ReviewUserPrivacy, ReviewUserPrivacyInfo } from './ReviewStyle';
 import { ReviewInfoType } from "../../recoil/review/review";
 
 // icons
@@ -10,35 +10,39 @@ import ImageDefault from "../../assets/images/image-default.png";
 
 const ReviewItem = ({
   review,
-  onPress
+  disabled = true,
 }: {
   review: ReviewInfoType,
-  onPress?: () => any
+  disabled?: boolean
 }) => {
 
-  const handleButtonPress = () => {
-    console.log(`${review.profileId} 버튼이 눌렸습니다.`);
-    if (onPress) {
-      onPress;
-    }
-  };
-
   return (
-    <ReviewContainer onPress={handleButtonPress}>
+    <ReviewContainer>
       <ReviewUserContainer>
-        <ReviewUserImage source={ImageDefault}></ReviewUserImage>
+        <ReviewUserImage
+          // source={(review?.profileImage && { uri: review.profileImage }) || ImageDefault}
+          source={{ uri: review.profileImage } || ImageDefault}
+        ></ReviewUserImage>
         <ReviewUserInfo>
           <ReviewUserInfoItem>
             <ReviewUserPrivacy>
               <ReviewUserPrivacyInfo>
-                <ReviewTitle>{review.profileId}</ReviewTitle>
+                <ReviewTitle>{review.nickname}</ReviewTitle>
                 <ReviewSubContent>{review.performedDate}</ReviewSubContent>
               </ReviewUserPrivacyInfo>
-              <Text>{review.rating}</Text>
+              <Text>{review.rating} 점</Text>
             </ReviewUserPrivacy>
-            <ReviewIcon source={Key}></ReviewIcon>
+            {
+              review.isVerified &&
+              <ReviewIcon source={Key}></ReviewIcon>
+            }
           </ReviewUserInfoItem>
-          <ReviewContent>{review.content}</ReviewContent>
+          {
+            disabled ?
+              <ReviewContent>{review.content}</ReviewContent>
+              :
+              <ReviewContentDisabled>{review.content}</ReviewContentDisabled>
+          }
         </ReviewUserInfo>
       </ReviewUserContainer>
     </ReviewContainer>
